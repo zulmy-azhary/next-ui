c912c038fae994e3b763ec40e1aa1f113b0ac6b6
zoel-a <zulmyazhary32@gmail.com>
2023-06-06 22:25:18 +0800

    Modify site config

--- config/site.ts
+++ config/site.ts
@@ -1,18 +1,17 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "Next UI",
   description:
     "Beautifully designed components built with Radix UI and Tailwind CSS.",
   mainNav: [
     {
       title: "Home",
-      href: "/",
+      href: "/main",
+    },
+    {
+      title: "About",
+      href: "/main/about",
     },
   ],
-  links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
-  },
 }
